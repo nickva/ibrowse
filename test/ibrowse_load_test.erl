2772261a3bbeeb25edc6b009a7d754d9a397ad74
Nick Vatamaniuc <nickva@users.noreply.github.com>
2025-09-24 16:34:20 -0400

    Bump min erlang ver and add CI (#181)
    * Add Github Actions CI tests - Travis CI doesn't work any longer
    * Update for Erlang version >= 25

--- test/ibrowse_load_test.erl
+++ test/ibrowse_load_test.erl
@@ -1,7 +1,6 @@
 -module(ibrowse_load_test).
 %%-compile(export_all).
 -export([
-         random_seed/0,
          start/3,
          query_state/0,
          shutdown/0,
@@ -14,20 +13,6 @@
          worker_loop/2,
          update_unknown_counter/2
         ]).
-
--ifdef(new_rand).
-
--define(RAND, rand).
-random_seed() ->
-	ok.
-
--else.
-
--define(RAND, random).
-random_seed() ->
-	random:seed(os:timestamp()).
-
--endif.
 
 -define(ibrowse_load_test_counters, ibrowse_load_test_counters).
 
@@ -123,7 +108,6 @@ spawn_workers(0, _Num_requests, _Parent, Acc) ->
     lists:reverse(Acc);
 spawn_workers(Num_workers, Num_requests, Parent, Acc) ->
     Pid_ref = spawn_monitor(fun() ->
-                                    random_seed(),
                                     case catch worker_loop(Parent, Num_requests) of
                                         {'EXIT', Rsn} ->
                                             io:format("Worker crashed with reason: ~p~n", [Rsn]);
@@ -163,7 +147,7 @@ wait_for_workers([{Pid, Pid_ref} | T] = Pids) ->
 worker_loop(Parent, 0) ->
     Parent ! {done, self()};
 worker_loop(Parent, N) ->
-    Delay = ?RAND:uniform(100),
+    Delay = rand:uniform(100),
     Url = case Delay rem 10 of
               %% Change 10 to some number between 0-9 depending on how
               %% much chaos you want to introduce into the server
